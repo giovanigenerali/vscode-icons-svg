f096c35a3ea294db38e67b533e37f27b974730eb
Giovani Generali <giovani.generali@gmail.com>
2020-08-23 22:48:42 -0300

    update

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ const { extname } = require("path");
 const { getIconForFile } = require("vscode-icons-js");
 
 export function vscodeIconSVG(fileName) {
+  const sourceUrl =
+    "https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/";
   const iconFallback = "default_file.svg";
-
   let iconFileExtension = iconFallback;
 
-  const extension =
-    extname(fileName) || fileName.indexOf(".") !== -1
-      ? fileName
-      : `.${fileName}`;
+  if (fileName) {
+    const extension =
+      extname(fileName) || fileName.indexOf(".") !== -1
+        ? fileName
+        : `.${fileName}`;
 
-  iconFileExtension = getIconForFile(extension);
+    iconFileExtension = getIconForFile(extension);
+  }
 
-  const url = `https://raw.githubusercontent.com/vscode-icons/vscode-icons/master/icons/${iconFileExtension}`;
-
-  return url;
+  return `${sourceUrl}${iconFileExtension}`;
 }
