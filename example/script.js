d3bb972afb614143bc178c9c05185191240d7ab3
Giovani Generali <giovani.generali@gmail.com>
2020-08-24 00:21:23 -0300

    add example

---
+++ example/script.js
@@ -0,0 +1,59 @@
+const js = document.createElement("IMG");
+js.src = vscodeIconSVG("file.js");
+js.alt = "Icon file.js";
+js.loading = "lazy";
+document.body.appendChild(js);
+
+const jsx = document.createElement("IMG");
+jsx.src = vscodeIconSVG("file.jsx");
+jsx.alt = "Icon file.jsx";
+jsx.loading = "lazy";
+document.body.appendChild(jsx);
+
+const html = document.createElement("IMG");
+html.src = vscodeIconSVG("file.html");
+html.alt = "Icon file.html";
+html.loading = "lazy";
+document.body.appendChild(html);
+
+const gitignore = document.createElement("IMG");
+gitignore.src = vscodeIconSVG(".gitignore");
+gitignore.alt = "Icon .gitignore";
+gitignore.loading = "lazy";
+document.body.appendChild(gitignore);
+
+const packageJson = document.createElement("IMG");
+packageJson.src = vscodeIconSVG("package.json");
+packageJson.alt = "Icon package.json";
+packageJson.loading = "lazy";
+document.body.appendChild(packageJson);
+
+const eslintrc = document.createElement("IMG");
+eslintrc.src = vscodeIconSVG(".eslintrc");
+eslintrc.alt = "Icon .eslintrc";
+eslintrc.loading = "lazy";
+document.body.appendChild(eslintrc);
+
+const babel = document.createElement("IMG");
+babel.src = vscodeIconSVG(".babelrc");
+babel.alt = "Icon .babelrc";
+babel.loading = "lazy";
+document.body.appendChild(babel);
+
+const webpack = document.createElement("IMG");
+webpack.src = vscodeIconSVG("webpack.config.js");
+webpack.alt = "Icon webpack.config.js";
+webpack.loading = "lazy";
+document.body.appendChild(webpack);
+
+const dockerCompose = document.createElement("IMG");
+dockerCompose.src = vscodeIconSVG("docker-compose.yml");
+dockerCompose.alt = "Icon docker-compose.yml";
+dockerCompose.loading = "lazy";
+document.body.appendChild(dockerCompose);
+
+const github = document.createElement("IMG");
+github.src = vscodeIconSVG("github");
+github.alt = "Icon docker-compose.yml";
+github.loading = "lazy";
+document.body.appendChild(github);
